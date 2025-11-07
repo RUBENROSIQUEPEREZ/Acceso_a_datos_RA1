92e3c0cb42a534894dee655adfbd5df040f54f7f
RUBENROSIQUEPEREZ <5519386@alu.murciaeduca.es>
2025-11-07 08:23:45 +0100

    Tarea RA1

---
+++ src/main/java/es/cifpcarlos3/vo/Curso.java
@@ -0,0 +1,26 @@
+package es.cifpcarlos3.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class Curso implements Serializable {
+    @JacksonXmlProperty(isAttribute = true)
+    private String nombre;
+
+    @JacksonXmlElementWrapper(localName = "Lista_de_alumnos")
+    @JacksonXmlProperty(localName = "Alumno")
+    private List <Alumno> ListaAlumnos = new ArrayList<>();
+}
